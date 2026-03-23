1b0c82e218b99161eb7bed6847926eb1c5224d50
Sandeep0964 <120566017+Sandeep0964@users.noreply.github.com>
2026-03-23 13:08:00 +0530

    setup of app and components

---
+++ src/components/Results.tsx
@@ -0,0 +1,4 @@
+"use client";
+import { ForecastResponse, Coin } from "@/lib/api";
+import { HistoryChart, PredictionChart, ForecastChart } from "./Charts";
+import { TrendingUp, TrendingDown, Download } from "lucide-react";
